b5989b94f47102096a1d7048cf096884b9254c50
sohanwebdevctg <amirhossainsohan0@gmail.com>
2026-06-14 00:33:13 +0600

    create manager middleware and check successfully

--- src/middlewares/isManager.ts
+++ src/middlewares/isManager.ts
@@ -1,11 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 
-const isAdmin = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+const isManager = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 
   try {
 
     // get token user
-  const user = req.user;
+    const user = req.user;
 
     if (!user) {
       res.status(401).json({ 
@@ -16,10 +16,10 @@ const isAdmin = async (req: Request, res: Response, next: NextFunction): Promise
     }
 
     // check user role
-    if (user.userRoleName !== 'admin') {
+    if (user.userRoleName !== 'manager') {
       res.status(403).json({
         success: false,
-        message: 'Access Denied! Only admins can access this route.',
+        message: 'Access Denied! Only managers can access this route.',
       });
       return;
     }
@@ -29,10 +29,10 @@ const isAdmin = async (req: Request, res: Response, next: NextFunction): Promise
   } catch (error: any) {
     res.status(500).json({ 
       success: false, 
-      message: error.message || 'Server error in isAdmin guard.' 
+      message: error.message || 'Server error in isManager guard.' 
     });
     return;
   }
 };
 
-export default isAdmin;
+export default isManager;
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -3,15 +3,15 @@ import {registration, login, getAllUser} from '../controllers/userController';
 import {upload} from '../middlewares/uploadMiddleware';
 import verifyToken from '../middlewares/authMiddleware';
 import isAdmin from '../middlewares/adminMiddleware';
+import isManager from '../middlewares/isManager';
 
 const router = Router();
 
 // user router and controller
 router.post('/registration', upload.single('image'), registration);
 router.post('/login', login);
-router.get('/getAllUser', verifyToken, isAdmin, getAllUser);
-
-
+router.get('/getAllUserAdmin', verifyToken, isAdmin, getAllUser);
+router.get('/getAllUserManager', verifyToken, isManager, getAllUser);
 
 
 export default router;
